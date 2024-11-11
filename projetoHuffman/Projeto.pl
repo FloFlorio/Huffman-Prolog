2bbf7967ba7dcdb9692ccd24f3f001e7260d06da
floflorio <gfloriovieira@uol.com.br>
2024-11-11 19:41:13 -0300

    bem incompleto, mas ta indo

---
+++ projetoHuffman/Projeto.pl
@@ -0,0 +1,29 @@
+arvore([]).
+arvore([E,Esq,Dir]) :- arvore(Esq), arvore(Dir).
+
+contarFrequencia([],[]).
+contarFrequencia(Texto, F) :- 
+    sort(Texto, TextoOrd), 
+    contarFrequenciaOrd(TextoOrd,F).
+
+contarFrequenciaOrd([],[]).
+contarFrequenciaOrd([C|Cs],R) :- contarFrequenciaOrd(Cs,C,1,R).
+contarFrequenciaOrd([],C,F,[(C,F)]).
+contarFrequenciaOrd([C|Cs],C,F,R) :- F1 is F+1, contarFrequenciaOrd(Cs,C,F1,R).
+contarFrequenciaOrd([A|As],C,N,[(C,N)|Xs]) :- contarFrequenciaOrd(As,A,1,X).
+
+contarOcorrencias(0,_,[]).
+contarOcorrencias(N,X,[X|Xs]) :- N1 is N+1, (N1,X,Xs).
+contarOcorrencias(N,X,[Y|Xs]) :- X /= Y, contarOcorrencias(N,X,Xs).
+
+removerOcorrencias([],_,[]).
+removerOcorrencias([X|As], X, Xs) :- removerOcorrencias(As,X,Xs).
+removerOcorrencias([A|As], X, [A|Xs]) :- removerOcorrencias(As,X,Xs).
+
+criarFolha([],[]).
+criarFolha([C,F|Xs], [Leaf(C,F)|Ys]) :- criarFolha(Xs,Ys).
+
+construirArvore([],[]).
+
+peso(Leaf(_,F),F).
+peso(Node(F,Esq,Dir),F) :- peso(Esq,Fesq), peso(Dir,fdi), F is Fesq + Fdir.
